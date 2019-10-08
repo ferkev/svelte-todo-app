a5b4478156dbf16b95f57334275f879a56fc4cb9
kevin LE FRANC <kevin.le-franc-ext@socgen.com>
2019-10-08 13:47:23 +0200

    app ready for sharing

--- src/Todos.svelte
+++ src/Todos.svelte
@@ -21,25 +21,70 @@
 
   let newTodoTitle = "";
   let currentFilter = "all";
-  let nextId = todos.length + 1;
+  let nextId = 4;
 
   /**
-   * @desc : function for adding task
+   * @desc: function for adding task
    * @param: {event} e - the event when keydown enter is down
    **/
   const addTodo = e => {
     if (e.key === "Enter") {
-      todos = [...todos, { id: nextId, title: newTodoTitle, completed: false }];
+      todos = [
+        ...todos,
+        {
+          id: nextId,
+          title: newTodoTitle,
+          completed: false
+        }
+      ];
+      nextId += 1;
       newTodoTitle = "";
     }
   };
 
-
+  /**
+   * @desc: function to change the filter option
+   * @param: {string} newFilter - a filter to execute the selection
+   * @return: {string} currentFilter - the new filter choosen
+   **/
   const updateFilter = newFilter => {
-   currentFilter = newFilter
+    currentFilter = newFilter;
   };
 
+  /**
+   * @desc: function
+   * @param: {event} e - check event
+   **/
+  const checkAllTodos = e => {
+    todos.forEach(todo => todo.completed = e.target.checked);
+    todos = todos;
+  };
 
+  const clearCompleted = () => {
+    todos = todos.filter(todo => !todo.completed);
+  };
+
+  const handleDeleteTodo = e => {
+    todos = todos.filter(todo => todo.id !== e.detail.id);
+  };
+
+  const handleToggleComplete = e => {
+    const todoIndex = todos.findIndex(todo => {
+      return todo.id === e.detail.id;
+    });
+
+    const updatesTodo = {...todos[todoIndex], completed: !todos[todoIndex].completed};
+    todos = [
+      ...todos.slice(0, todoIndex),
+      updatesTodo,
+      ...todos.slice(todoIndex + 1)
+    ];
+  };
+
+  $: todosRemaining = filteredTodos.filter(todo => !todo.completed).length;
+  $: filteredTodos = currentFilter === "all" ? todos : currentFilter === "completed"
+      ? todos.filter(todo => todo.completed)
+      : todos.filter(todo => !todo.completed);
 </script>
 
 <style>
@@ -82,9 +127,9 @@
   button:focus {
     outline: none;
   }
-  /* .active {
-        background: lightseagreen;
-    } */
+  .active {
+    background: lightseagreen;
+  }
 </style>
 
 <div class="container">
@@ -111,7 +156,10 @@
   <div class="inner-container">
     <div>
       <label>
-        <input class="inner-container-input" type="checkbox" on:change={checkAllTodos}/>
+        <input
+          class="inner-container-input"
+          type="checkbox"
+          on:change={checkAllTodos} />
         Check All
       </label>
     </div>
@@ -120,9 +168,21 @@
 
   <div class="inner-container">
     <div>
-      <button on:click={() => updateFilter('all')} class:active="{currentFilter === 'all'}">All</button>
-      <button on:click={() => updateFilter('active')} class:active="{currentFilter === 'active'}">Active</button>
-      <button on:click={() => updateFilter('completed')} class:active="{currentFilter === 'completed'}">Completed</button>
+      <button
+        on:click={() => updateFilter('all')}
+        class:active={currentFilter === 'all'}>
+        All
+      </button>
+      <button
+        on:click={() => updateFilter('active')}
+        class:active={currentFilter === 'active'}>
+        Active
+      </button>
+      <button
+        on:click={() => updateFilter('completed')}
+        class:active={currentFilter === 'completed'}>
+        Completed
+      </button>
     </div>
     <dir>
       <button on:click={clearCompleted}>Clear Completed</button>
